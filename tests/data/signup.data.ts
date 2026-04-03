1d81deae8e0368c94ab7803fca695f7df0e2a0b3
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-03 17:59:04 +0300

    added comments regarding data files and adding account.page.ts

--- tests/data/signup.data.ts
+++ tests/data/signup.data.ts
@@ -1,1 +1,2 @@
 //this file contains data that is to be used to make new accounts
+//the other login.data.json file is actually used to store cookies and tokens and is not to be modified by the user
---
+++ tests/pages/account.page.ts
@@ -0,0 +1,10 @@
+import { Page } from "@playwright/test";
+
+export class Account{
+
+
+    constructor(page : Page){
+
+        
+    }
+}
