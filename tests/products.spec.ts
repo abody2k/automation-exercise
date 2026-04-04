45115f7f6bdaa936bc37f80ad03e9b97e25bfe9e
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-04 19:11:39 +0300

    testing trying to get products using post method API

--- tests/products.spec.ts
+++ tests/products.spec.ts
@@ -1,19 +1,28 @@
-import { getAllProducts } from "../api/products.api";
+import { getAllProducts, getAllProductsWithPost } from "../api/products.api";
 import { expect, test } from "../fixtures/global.fixture";
 
-test.describe("all products tests go here",()=>{
+test.describe("all products tests go here", () => {
 
-
-
-    test("get all products",async()=>{
+    test.skip("get all products", async () => {
 
 
         let data = await getAllProducts();
 
-       expect(data.responseCode).toBe(200);
-       expect(data.products).toBeTruthy();
-        
+        expect(data.responseCode).toBe(200);
+        expect(data.products).toBeTruthy();
+
     })
 
+
+
+    test("get all products with a post", async () => {
+
+
+        let data = await getAllProductsWithPost();
+
+        expect(data.responseCode).toBe(405);
+        expect(data.message).toBe("This request method is not supported.");
+
+    })
 
 })
