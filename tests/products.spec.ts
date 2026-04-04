68dee33b38ed7e6b889df333562ad23e112355ee
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-04 19:05:07 +0300

    made .env file globally configured before any test

--- tests/products.spec.ts
+++ tests/products.spec.ts
@@ -1,10 +1,5 @@
 import { getAllProducts } from "../api/products.api";
 import { expect, test } from "../fixtures/global.fixture";
-import path from "path";
-import dotenv from "dotenv"
-
-
-dotenv.config({ path: path.resolve(__dirname, "./.env") });
 
 test.describe("all products tests go here",()=>{
 
