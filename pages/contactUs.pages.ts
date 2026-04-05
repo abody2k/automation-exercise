acdd0e7a4c5aced8e9ff8abd38f9ac538a75095d
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-05 19:40:39 +0300

    implemented contact us goto function

--- pages/contactUs.pages.ts
+++ pages/contactUs.pages.ts
@@ -1,6 +1,19 @@
+import { Page } from "@playwright/test";
+import { Header } from "../components/header.component";
+
 export class ContactUs{
 
+    constructor(private page : Page){
 
+
+    }
+
+    async goto(header:Header){
+
+        await this.page.goto(process.env.BASE_URL);
+        await header.goToContactUs();
+
+    }
 
 
 
