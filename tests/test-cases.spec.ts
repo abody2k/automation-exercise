bf6c75fe14a043ee228a543c03b99dd21116c639
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-05 21:37:37 +0300

    implemented go home that takes user to home page

--- tests/test-cases.spec.ts
+++ tests/test-cases.spec.ts
@@ -1,4 +1,5 @@
 import { expect, test } from "../fixtures/global.fixture";
+import { goHome } from "../utils/home.util";
 
 test.describe("testing test cases page", () => {
 
@@ -6,7 +7,7 @@ test.describe("testing test cases page", () => {
 
     test("verify that test cases page is functional and user can go there", async ({header,page}) => {
 
-        await header.
+        await goHome(page);
         await header.goToTestCases();
         await expect(page).toHaveURL(/.*test_cases/);
     })
