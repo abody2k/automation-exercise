132fa91a6efb3141f117c90693bd5a5654262920
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-05 22:23:23 +0300

    Finished testing products list and viewing one product plus testing its visibility

--- tests/products-ui.spec.ts
+++ tests/products-ui.spec.ts
@@ -13,6 +13,18 @@ test.describe("All products UI tests goes here",()=>{
         await expect(prodcuts.productsList).toBeVisible();
         await prodcuts.clickOnFirstProduct();
         await expect(page).toHaveURL(/.*\/product_details\/1/)
+        let items = [prodcuts.getItemAvailability()
+        ,prodcuts.getItemBrand()
+        ,prodcuts.getItemCategory()
+        ,prodcuts.getItemCondition()
+        ,prodcuts.getItemPrice()
+        ,prodcuts.getItemQuantity()
+        ,prodcuts.getItemTitle()]
+
+        for(let i =0;i<items.length;i++){
+
+            await expect(items[i]).toBeVisible();
+        }
         
 
 
