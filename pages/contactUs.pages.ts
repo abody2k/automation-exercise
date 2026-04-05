2e26741e02fde36bc144257eae4890312704b9bd
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-05 20:07:28 +0300

    added success msg locator

--- pages/contactUs.pages.ts
+++ pages/contactUs.pages.ts
@@ -2,56 +2,56 @@ import { Locator, Page } from "@playwright/test";
 import { Header } from "../components/header.component";
 import path from "path";
 
-export class ContactUs{
+export class ContactUs {
 
 
 
-    getInTouch : Locator
-    
-    constructor(private page : Page){
+    getInTouch: Locator
+    successMsg: Locator
+
+    constructor(private page: Page) {
 
         this.getInTouch = this.page.getByText("Get In Touch")
+        this.successMsg = this.page.locator('#contact-page').getByText('Success! Your details have')
     }
 
-    async goto(){
+    async goto() {
 
         await this.page.goto(process.env.BASE_URL);
 
     }
 
-    async clickOnAndFillField(name:string,content:string){
+    async clickOnAndFillField(name: string, content: string) {
 
-        let selector = this.page.getByRole('textbox',{name:name})
+        let selector = this.page.getByRole('textbox', { name: name })
         await selector.click();
         await selector.fill(content)
     }
-    async fillName(name : string){
-        
-        await this.clickOnAndFillField('Name',name)
+    async fillName(name: string) {
+
+        await this.clickOnAndFillField('Name', name)
     }
 
-    async fillEmail(email : string){
+    async fillEmail(email: string) {
 
-        await this.clickOnAndFillField('Email',email)
+        await this.clickOnAndFillField('Email', email)
     }
 
-    async fillSubject(subject : string){
+    async fillSubject(subject: string) {
 
-        await this.clickOnAndFillField('Subject',subject)
+        await this.clickOnAndFillField('Subject', subject)
     }
 
 
-    async uploadFile(file:File){
+    async uploadFile(file: File) {
 
         let selector = this.page.getByRole('button', { name: 'Choose File' })
         await selector.click()
-        selector.setInputFiles(path.resolve(__dirname,"icon.ico"))
+        await selector.setInputFiles(path.resolve(__dirname, "icon.ico"))
     }
 
 
-
-
-
-
-    
+    async submit() {
+        await this.page.getByRole('button', { name: 'Submit' }).click()
+    }
 }
