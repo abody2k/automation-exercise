108b9aa63d2ed877908fca763a7b3e7e1cb128c9
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-06 16:26:13 +0300

    added home page file and modified the testing category spec

---
+++ pages/home.page.ts
@@ -0,0 +1,13 @@
+import { Page } from "@playwright/test";
+
+export class Home{
+
+
+    constructor(private page: Page){
+
+
+
+    }
+
+    
+}
--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,8 +1,15 @@
 import { test } from "../fixtures/global.fixture";
+import { goHome } from "../utils/home.util";
 
-test.describe("Home tests go here",()=>{
+test.describe("Home tests go here", () => {
 
 
 
-    
+    test("Check the visibility of the categories", async({page}) => {
+
+
+
+        await goHome(page);
+        
+    })
 })
