d5f0b4556e2f8909636456acf83985d82ff17961
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-07 18:27:43 +0300

    get address1 and address 2 was implemented

--- pages/checkout.page.ts
+++ pages/checkout.page.ts
@@ -38,6 +38,12 @@ export class Checkout {
     }
 
 
+    getAddress1Address2() {
+
+
+        return this.getLocatorUsingAddressDelivery(".address_address1").nth(1) // for some reason, the dev decided to duplicate the field 3 times so the right one is the second field hence the nth(1)
+    }
+
     getLocatorUsingAddressDelivery(locator_class: string) {
 
         return this.addressDelivery.locator(locator_class)
