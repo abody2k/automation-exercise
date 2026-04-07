60e0104fd2537e0f97a6800cf65315d8ff37025c
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-07 17:53:37 +0300

    get firstname in delivery address info

--- pages/checkout.page.ts
+++ pages/checkout.page.ts
@@ -1,6 +1,16 @@
+import { Page } from "@playwright/test";
+
 export class Checkout{
 
+    constructor(private page: Page){
+
+    }
+
+    getFirstNameInDeliveryAddress(){
 
 
-    
+        return this.page.locator("#address_delivery").locator(".address_firstname")
+    }
+
+
 }
