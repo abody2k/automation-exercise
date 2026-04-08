68d51519a20be53cb8cf9fcb8a4554a0f4b9a19d
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-08 18:18:20 +0300

    finished Adding a comment then placing the order

--- e2e/checkout.spec.ts
+++ e2e/checkout.spec.ts
@@ -2,7 +2,7 @@ import { data } from "../data/account.data";
 import { productsNames } from "../data/products.data";
 import { expect, test } from "../fixtures/global.fixture";
 import { makeNewAccount } from "../flows/auth.flow";
-import { addProductAndProceedToCheckout } from "../flows/products.flow";
+import { addProductAndProceedToCheckout, checkIfAdressInfoIsCorrect } from "../flows/products.flow";
 
 test.describe("e2e tests", () => {
 
@@ -31,7 +31,7 @@ test.describe("e2e tests", () => {
 
         await test.step("Checking address info in checkout page", async () => {
             await expect(page).toHaveURL(/.*checkout/)
-
+            checkIfAdressInfoIsCorrect({ checkout, data })
 
         })
 
@@ -50,7 +50,7 @@ test.describe("e2e tests", () => {
 
 
 
-    //This is case 25
+    //This is case 26
     test("Making a payment and downloading the invoice after making an account", async ({ home, header, account, page, products, checkout }) => {
 
 
@@ -78,15 +78,14 @@ test.describe("e2e tests", () => {
 
         await test.step("Checking address info in checkout page", async () => {
             await expect(page).toHaveURL(/.*checkout/)
-            let addressType: ["biling", "delivery"] = ["biling", "delivery"]
-            for (const address of addressType) {
+            checkIfAdressInfoIsCorrect({ checkout, data })
 
-                await expect(checkout.getFirstNameLastName(address)).toContainText(`${data.firstName} ${data.lastName}`)
-                await expect(checkout.getPhoneNumber(address)).toContainText(`${data.mobileNumber}`)
-                await expect(checkout.getAddress1Address2(address)).toContainText(`${data.address}`)
-                await expect(checkout.getCityStatePostcode(address)).toContainText(`${data.city} ${data.state} ${data.zipCode}`)
-                await expect(checkout.getCountry(address)).toContainText(`${data.country}`)
-            }
+        })
+
+        await test.step("Adding a comment then placing the order",async()=>{
+
+            await checkout.leaveComment("RANDOM COMMENT")
+            await checkout.clickOnPlaceOrder();
 
         })
 
