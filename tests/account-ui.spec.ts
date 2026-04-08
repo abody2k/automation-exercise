765fc105baf72de6f338056c6b1856bee8ea5190
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-08 10:32:05 +0300

    split the E2E test into multiple test steps

--- tests/account-ui.spec.ts
+++ tests/account-ui.spec.ts
@@ -91,33 +91,56 @@ test.describe("All account UI related test.skips", () => {
   //This is case 23
   test("Verify address details in checkout page after making an account", async ({ home, header, account, page, products, checkout }) => {
 
-    await home.goHome();
-    await header.goToSignupLogin();
-    await makeNewAccount({ account, page, data });
 
-    await account.clickOnContinueAfterMakingAccount();
-    await expect(account.loggedInLabel).toBeVisible();
+    await test.step("Going home", async () => {
 
-    //we are at home page right now
+      await home.goHome();
 
-    await products.addProductToCartByName(productsNames[0])
-    await products.viewCartAfterAddingItem();
-    await products.clickOnProceedToCheckout();
+    })
 
-    await expect(page).toHaveURL(/.*checkout/)
+    await test.step("Going to sign in/up and making a new account", async () => {
 
-    let addressType: ["biling", "delivery"] = ["biling", "delivery"]
-    for (const address of addressType) {
+      await header.goToSignupLogin();
+      await makeNewAccount({ account, page, data });
+      await account.clickOnContinueAfterMakingAccount();
+      await expect(account.loggedInLabel).toBeVisible();
+    })
+    await test.step("Adding product and proceeding to checkout", async () => {
 
-      await expect(checkout.getFirstNameLastName(address)).toContainText(`${data.firstName} ${data.lastName}`)
-      await expect(checkout.getPhoneNumber(address)).toContainText(`${data.mobileNumber}`)
-      await expect(checkout.getAddress1Address2(address)).toContainText(`${data.address}`)
-      await expect(checkout.getCityStatePostcode(address)).toContainText(`${data.city} ${data.state} ${data.zipCode}`)
-      await expect(checkout.getCountry(address)).toContainText(`${data.country}`)
-    }
-    await header.DeleteAccount();
-    await expect(account.accountDeletedMsg).toBeVisible();
-    await account.clickOnContinueAfterDeletingTheAccount();
+      //we are at home page right now
+
+      await products.addProductToCartByName(productsNames[0])
+      await products.viewCartAfterAddingItem();
+      await products.clickOnProceedToCheckout();
+
+    })
+
+
+
+    await test.step("Checking address info in checkout page", async () => {
+      await expect(page).toHaveURL(/.*checkout/)
+      let addressType: ["biling", "delivery"] = ["biling", "delivery"]
+      for (const address of addressType) {
+
+        await expect(checkout.getFirstNameLastName(address)).toContainText(`${data.firstName} ${data.lastName}`)
+        await expect(checkout.getPhoneNumber(address)).toContainText(`${data.mobileNumber}`)
+        await expect(checkout.getAddress1Address2(address)).toContainText(`${data.address}`)
+        await expect(checkout.getCityStatePostcode(address)).toContainText(`${data.city} ${data.state} ${data.zipCode}`)
+        await expect(checkout.getCountry(address)).toContainText(`${data.country}`)
+      }
+
+    })
+
+
+
+    await test.step("Deleteing account", async () => {
+
+      await header.DeleteAccount();
+      await expect(account.accountDeletedMsg).toBeVisible();
+      await account.clickOnContinueAfterDeletingTheAccount();
+    })
+
+
 
   })
 
