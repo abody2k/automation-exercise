9ec80a9bbdfd5e5f4f57dcde63d655b492196bf8
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-08 18:14:15 +0300

    implemented checkIfAdressInfoIsCorrect

--- flows/products.flow.ts
+++ flows/products.flow.ts
@@ -1,6 +1,8 @@
+import { expect } from "../fixtures/global.fixture";
+import { Checkout } from "../pages/checkout.page";
 import { Products } from "../pages/products.page";
 
-export async function addProductAndProceedToCheckout({productName,products}:{productName: string,products:Products}) {
+export async function addProductAndProceedToCheckout({ productName, products }: { productName: string, products: Products }) {
 
 
     await products.addProductToCartByName(productName)
@@ -8,3 +10,17 @@ export async function addProductAndProceedToCheckout({productName,products}:{pro
     await products.clickOnProceedToCheckout();
 }
 
+
+export async function checkIfAdressInfoIsCorrect({ checkout, data }: { checkout: Checkout, data: { firstName: string, lastName: string, mobileNumber: string, zipCode: string, city: string, country: string, address: string, state: string } }) {
+
+    let addressType: ["biling", "delivery"] = ["biling", "delivery"]
+    for (const address of addressType) {
+
+        await expect(checkout.getFirstNameLastName(address)).toContainText(`${data.firstName} ${data.lastName}`)
+        await expect(checkout.getPhoneNumber(address)).toContainText(`${data.mobileNumber}`)
+        await expect(checkout.getAddress1Address2(address)).toContainText(`${data.address}`)
+        await expect(checkout.getCityStatePostcode(address)).toContainText(`${data.city} ${data.state} ${data.zipCode}`)
+        await expect(checkout.getCountry(address)).toContainText(`${data.country}`)
+    }
+
+}
